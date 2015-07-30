f04fc58b401dc36709a63ccd2af4908358e66d85
Tobias Schulte <tobias.schulte@gliderpilot.de>
2015-07-30 22:28:37 +0200

    test(core): test if plugin can be applied

---
+++ src/test/groovy/de/gliderpilot/gradle/semanticrelease/GradleSemanticReleasePluginSpec.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.gliderpilot.gradle.semanticrelease
+
+import nebula.test.ProjectSpec
+
+/**
+ * Created by tobias on 7/2/15.
+ */
+class GradleSemanticReleasePluginSpec extends ProjectSpec {
+
+    private static final String PLUGIN = 'de.gliderpilot.semantic-release'
+    private static final String BASE_PLUGIN = 'de.gliderpilot.semantic-release-base'
+
+    def 'apply does not throw exceptions'() {
+        when:
+        project.apply plugin: pluginName
+
+        then:
+        noExceptionThrown()
+
+        where:
+        pluginName << [PLUGIN, BASE_PLUGIN]
+    }
+
+    def 'apply is idempotent'() {
+        when:
+        project.apply plugin: pluginName
+        project.apply plugin: pluginName
+
+        then:
+        noExceptionThrown()
+
+        where:
+        pluginName << [PLUGIN, BASE_PLUGIN]
+    }
+
+}
