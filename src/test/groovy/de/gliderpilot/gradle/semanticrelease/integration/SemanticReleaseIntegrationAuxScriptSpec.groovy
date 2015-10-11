3f3e049677d9cd8c684271b631d7efe87db1acd5
rzabini <romano.zabini@gmail.com>
2015-10-11 09:19:24 +0200

    chore: add test for plugin application from external gradle script

---
+++ src/test/groovy/de/gliderpilot/gradle/semanticrelease/integration/SemanticReleaseIntegrationAuxScriptSpec.groovy
@@ -0,0 +1,49 @@
+package de.gliderpilot.gradle.semanticrelease.integration
+
+import de.gliderpilot.gradle.semanticrelease.SemanticReleasePlugin
+import de.gliderpilot.gradle.semanticrelease.SemanticReleasePluginIntegrationSpec
+import nebula.test.IntegrationSpec
+import nebula.test.functional.ExecutionResult
+import spock.lang.Specification
+
+class SemanticReleaseIntegrationAuxScriptSpec extends SemanticReleasePluginIntegrationSpec {
+
+    @Override
+    void setupGradleProject(){
+        buildFile << '''
+            apply from:'release.gradle'
+            println version
+        '''
+        setupGitignore()
+
+        file('release.gradle') << """
+            buildscript{
+
+                repositories{
+                    jcenter()
+                }
+
+                dependencies{
+                    classpath files('${getPluginCompileDir()}')
+                    classpath files('${getPluginCompileDir().replace('/classes/','/resources/')}')
+                    classpath "org.ajoberstar:gradle-git:1.3.0"
+                    classpath 'com.jcabi:jcabi-github:0.23'
+                }
+
+            }
+
+            apply plugin: de.gliderpilot.gradle.semanticrelease.SemanticReleasePlugin
+
+"""
+        runTasksSuccessfully(':wrapper')
+    }
+
+    def getPluginCompileDir(){
+        URL url=SemanticReleasePlugin.class.getResource(SemanticReleasePlugin.class.getSimpleName() + ".class")
+        String classFilePath=new File(url.toURI()).absolutePath
+        if(isWindows())
+            classFilePath = classFilePath.replace('\\','/')
+        String classFileRelative = SemanticReleasePlugin.class.getName().replace('.', '/') + ".class"
+        classFilePath-classFileRelative
+    }
+}
